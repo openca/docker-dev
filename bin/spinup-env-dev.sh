2527edb971ac4ae1da3536d80c144bf2dbf46771
Massimiliano Pala <m.pala@cablelabs.com>
2022-06-27 08:28:07 -0600

    Updated composer file to import the current director in the DEV container.

--- bin/spinup-env-dev.sh
+++ bin/spinup-env-dev.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo
-echo "Docker Spinup Script for ACM:C - v2.0.0"
-echo "(c) Massimiliano Pala and CableLabs, Inc."
+echo "Docker Spinup Script for DEV Environments - v1.0.0"
+echo "(c) Massimiliano Pala and OpenCA Labs"
 echo "All Rights Reserved"
 echo
 
@@ -13,17 +13,12 @@ PULL_IMAGES=no
 
 # Second argument to pull the images
 if [ "x$1" = "xy" -o "x$1" = "xyes" ] ; then
-  PULL_IMAGES=yes
+  PULL_IMAGES=no
 fi
 
 # Some info
 echo "   NOTE: USING DOCKER TAG (VERSION): $DOCKER_IMAGE_VERSION"
 echo
-
-# set script ENVs
-# IMG_MYSQL=code.cablelabs.com:4567/acm-project/acm-gateway/mysql:latest
-# IMG_REDIS=code.cablelabs.com:4567/acm-project/acm-gateway/redis:latest
-# IMG_DEV=code.cablelabs.com:4567/acm-project/acm-gateway/dev:$ACM_DOCKER_VERSION
 
 # All Images that need to be mounted
 IMG_CENTOS7_DEV=$DOCKER_BASE_IMAGE/centos7:$DOCKER_IMAGE_VERSION
@@ -34,22 +29,24 @@ NAME_NETWORK=dev-network
 
 # pull down images
 if [ "$PULL_IMAGES" = "yes" ] ; then
-  # docker pull $IMG_MYSQL
-  # docker pull $IMG_REDIS
-  docker pull $IMG_CENTOS7_DEV 2>/dev/null >/dev/null
+  docker pull $IMG_CENTOS7_DEV
 fi
 
 # Shutdown Running Instances
-docker container rm -f $NAME_CONTAINER_CENTOS7_DEV 2>/dev/null >/dev/null || true
-docker network rm $NAME_NETWORK 2>/dev/null >/dev/null || true
+docker container rm -f $NAME_CONTAINER_CENTOS7_DEV || true
+docker network rm $NAME_NETWORK || true
 
 # create user-defined bridge network if it doesn't exist yet
-docker network create --driver bridge $NAME_NETWORK 2>/dev/null >/dev/null || true
+docker network create --driver bridge $NAME_NETWORK || true
 
 # start dev container
+echo docker run --network $NAME_NETWORK --name $NAME_CONTAINER_CENTOS7_DEV \
+   --mount type=bind,src="$(pwd)",target=/workspace -p 127.0.0.1:80:8000 \
+   -d $IMG_CENTOS7_DEV
+
 docker run --network $NAME_NETWORK --name $NAME_CONTAINER_CENTOS7_DEV \
   --mount type=bind,src="$(pwd)",target=/workspace -p 127.0.0.1:80:8000 \
-  -d $IMG_CENTOS7_DEV tail -f /dev/null 2>/dev/null
+  -d $IMG_CENTOS7_DEV
 
 # Aliases to access the Bash on each container
 echo

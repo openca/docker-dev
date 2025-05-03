95c358c1eeb87cb9bec764efdaa917046121d9e5
Dr. Pala <madwolf@openca.org>
2025-05-03 19:39:32 -0400

    Updated docker-dev.sh script to check for the  existance. Minor editorial changes.

--- bin/docker-dev.sh
+++ bin/docker-dev.sh
@@ -10,6 +10,12 @@ if [ "x$PROJECT_NAME" = "x" ] ; then
 	PROJECT_NAME=unspecified-dev
 fi
 
+# Check the dev network is available
+if [ "$(docker network ls | grep devnet)" = "" ] ; then
+  # Create the dev network
+  docker network create devnet
+fi
+
 # Runs the container
 docker container run -d --rm --tty \
   -v "$PWD:/workspace" --network devnet \
